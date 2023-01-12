db3550b1d4cf3d8d0e6eb275e1b9a538aa674e71
JacobHu0723 <63916353+JacobHu0723@users.noreply.github.com>
2023-01-12 11:48:16 +0800

    Update UserScript.js
    V0.2

--- UserScript.js
+++ UserScript.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         自动刷MS Rewards
-// @version      0.1
+// @version      0.2
 // @author       Jacob Hu
 // @match        https://cn.bing.com/*
 // @grant        none
@@ -8,46 +8,74 @@
 
 (function() {
     'use strict';
+//手机版
+function mobile_rewards() {
+    var link;
+    var n;
+    var l;
+    var i=1;
+    //跳转函数
+    function locate() {
+    location.href=link;
+};
+    //打开显示Rewards
+    function open() {
+        document.getElementById("mHamburger").click();
+    };
+    while(i<=24){//循环条件
+    //设置链接
+    n=Math.ceil(Math.random() * 100);
+    l="https://cn.bing.com/search?q=";
+    link=l+n;
+
+    setTimeout(open,3000);
+    setTimeout(locate,7000);
+
+    i++;//增量
+    console.log(i);
+};
+}
+
+//电脑版
+function pc_rewards() {
+    var link;
+    var n;
+    var l;
+    var i=1;
+    //跳转函数
+    function locate() {
+    location.href=link;
+};
+
+    while(i<=30){//循环条件
+    //设置链接
+    n=Math.ceil(Math.random() * 100);
+    l="https://cn.bing.com/search?q=";
+    link=l+n;
+
+    setTimeout(locate,7000);
+
+    i++;//增量
+    console.log(i);
+};
+}
+
 //添加按钮
-//移动端
-    
+//移动版
 var inputmobilebtn = document.createElement('input');
 inputmobilebtn.id="mobile_btn";
 inputmobilebtn.type="button";
-inputmobilebtn.value="开始";
-inputmobilebtn.onclick = function() {
-    document.getElementById("sb_form_q").value = "13Hello";
-    document.querySelector(".sb_search button").click();
-};
+inputmobilebtn.value="手机版";
+inputmobilebtn.onclick = mobile_rewards()
 document.getElementById("b_header").appendChild(inputmobilebtn);
 
-
-
-
+//电脑版
 var inputpcbtn = document.createElement('input');
 inputpcbtn.id="pc_btn";
 inputpcbtn.type="button";
-inputpcbtn.value="电脑端";
-inputpcbtn.onclick = function() {
-    var input=document.querySelector('#sb_form_q');
-    input.value="23";
-                    };
+inputpcbtn.value="电脑版";
+inputpcbtn.onclick = pc_rewards()
 document.getElementById("b_header").appendChild(inputpcbtn);
-
-
-//var first=document.body.firstChild;
-//document.body.insertBefore(inputmobilebtn,first);
-//document.body.insertBefore(inputpcbtn,first);
-
-//电脑端
-function pc_rewards() {
-    var input=document.querySelector('#sb_form_q');
-    input.value="23";
-
-
-//移动端
-function mobile_rewards() {
-    document.getElementById("sb_form_q").value = "13Hello";
 
 
     // Your code here...
