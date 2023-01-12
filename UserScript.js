a29449306c839432bb07c725751c6badf333c429
JacobHu0723 <63916353+JacobHu0723@users.noreply.github.com>
2023-01-12 12:06:19 +0800

    Update UserScript.js
    手机电脑二合一

--- UserScript.js
+++ UserScript.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         自动刷MS Rewards
-// @version      0.2
+// @version      0.3
 // @author       Jacob Hu
 // @match        https://cn.bing.com/*
 // @grant        none
@@ -8,8 +8,8 @@
 
 (function() {
     'use strict';
-//手机版
-function mobile_rewards() {
+
+function rewards() {
     var link;
     var n;
     var l;
@@ -28,55 +28,21 @@ function mobile_rewards() {
     l="https://cn.bing.com/search?q=";
     link=l+n;
 
-    setTimeout(open,3000);
-    setTimeout(locate,7000);
+    setTimeout(open,7000);
+    setTimeout(locate,10000);
 
     i++;//增量
-    console.log(i);
-};
-}
 
-//电脑版
-function pc_rewards() {
-    var link;
-    var n;
-    var l;
-    var i=1;
-    //跳转函数
-    function locate() {
-    location.href=link;
-};
-
-    while(i<=30){//循环条件
-    //设置链接
-    n=Math.ceil(Math.random() * 100);
-    l="https://cn.bing.com/search?q=";
-    link=l+n;
-
-    setTimeout(locate,7000);
-
-    i++;//增量
-    console.log(i);
 };
 }
 
 //添加按钮
-//移动版
-var inputmobilebtn = document.createElement('input');
-inputmobilebtn.id="mobile_btn";
-inputmobilebtn.type="button";
-inputmobilebtn.value="手机版";
-inputmobilebtn.onclick = mobile_rewards()
-document.getElementById("b_header").appendChild(inputmobilebtn);
-
-//电脑版
-var inputpcbtn = document.createElement('input');
-inputpcbtn.id="pc_btn";
-inputpcbtn.type="button";
-inputpcbtn.value="电脑版";
-inputpcbtn.onclick = pc_rewards()
-document.getElementById("b_header").appendChild(inputpcbtn);
-
+var inputbtn = document.createElement('input');
+inputbtn.id="btn";
+inputbtn.type="button";
+inputbtn.value="开始";
+inputbtn.onclick = rewards()
+document.getElementById("b_header").appendChild(inputbtn);
 
     // Your code here...
 })();
