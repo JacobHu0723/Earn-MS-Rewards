bc51372d8b6f3f7f3bb51559641635ac834765d0
JacobHu0723 <63916353+JacobHu0723@users.noreply.github.com>
2023-01-12 13:51:47 +0800

    Update UserScript.js

--- UserScript.js
+++ UserScript.js
@@ -8,41 +8,57 @@
 
 (function() {
     'use strict';
-
-function rewards() {
-    var link;
-    var n;
-    var l;
-    var i=1;
-    //跳转函数
-    function locate() {
-    location.href=link;
-};
-    //打开显示Rewards
-    function open() {
-        document.getElementById("mHamburger").click();
+    function ale() {
+        alert()
     };
-    while(i<=24){//循环条件
-    //设置链接
-    n=Math.ceil(Math.random() * 100);
-    l="https://cn.bing.com/search?q=";
-    link=l+n;
 
-    setTimeout(open,7000);
-    setTimeout(locate,10000);
+    function rewards() {
+        var link;
+        var n;
+        var l;
+        var i=1;
+        //跳转函数
+        function locate() {
+            location.href=link;
+        };
+        //打开显示Rewards
+        function open() {
+            document.getElementById("mHamburger").click();
+        };
+        while(i<=25){//循环条件
+            //设置链接
+            n=Math.ceil(Math.random() * 100);
+            l="https://cn.bing.com/search?q=";
+            link=l+n;
 
-    i++;//增量
+            setTimeout(open,7000);
+            setTimeout(locate,10000);
 
-};
-}
+            i++;//增量
 
-//添加按钮
-var inputbtn = document.createElement('input');
-inputbtn.id="btn";
-inputbtn.type="button";
-inputbtn.value="开始";
-inputbtn.onclick = rewards()
-document.getElementById("b_header").appendChild(inputbtn);
+        };
+    }
 
-    // Your code here...
-})();
+        //添加按钮
+        //开始
+        var inputbtn = document.createElement('input');
+        inputbtn.id="btn";
+        inputbtn.type="button";
+        inputbtn.value="开始";
+        inputbtn.onclick = rewards()
+        document.getElementById("b_header").appendChild(inputbtn);
+
+        //停止
+        var stopbtn = document.createElement('input');
+        stopbtn.id="stop_btn";
+        stopbtn.type="button";
+        stopbtn.value="停止";
+        stopbtn.onclick = function() {
+            window.open("about:blank","_self")
+            window.close();
+            alert("请手动关闭标签页!");
+        };
+        document.getElementById("b_header").appendChild(stopbtn);
+
+        // Your code here...
+    })();
