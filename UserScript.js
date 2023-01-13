a90e0a364ff218a1b41c6650c003fd5a6d0ff41d
JacobHu0723 <63916353+JacobHu0723@users.noreply.github.com>
2023-01-13 14:50:51 +0800

    Update UserScript.js

--- UserScript.js
+++ UserScript.js
@@ -1,6 +1,8 @@
 // ==UserScript==
 // @name         自动刷MS Rewards
-// @version      0.3
+// @description  自动刷Microsoft Rewards
+// @icon         https://github.com/JacobHu0723/Earn-MS-Rewards/blob/master/Jing.png
+// @version      0.4
 // @author       Jacob Hu
 // @match        https://cn.bing.com/*
 // @grant        none
@@ -8,15 +10,12 @@
 
 (function() {
     'use strict';
-    function ale() {
-        alert()
-    };
+    var i=1;
 
     function rewards() {
         var link;
         var n;
         var l;
-        var i=1;
         //跳转函数
         function locate() {
             location.href=link;
@@ -25,40 +24,44 @@
         function open() {
             document.getElementById("mHamburger").click();
         };
-        while(i<=25){//循环条件
-            //设置链接
-            n=Math.ceil(Math.random() * 100);
-            l="https://cn.bing.com/search?q=";
-            link=l+n;
 
-            setTimeout(open,7000);
-            setTimeout(locate,10000);
+        //i=1
+        setTimeout(function(){
+            while(i<=25){//循环条件
+                //设置链接
+                n=Math.ceil(Math.random() * 999);
+                l="https://cn.bing.com/search?q=";
+                link=l+n;
+                setTimeout(open,0);
+                setTimeout(locate,2000);
+                if(i>25) {return}
 
-            i++;//增量
+                i++;//增量
 
-        };
+            };
+        },3000)
     }
 
-        //添加按钮
-        //开始
-        var inputbtn = document.createElement('input');
-        inputbtn.id="btn";
-        inputbtn.type="button";
-        inputbtn.value="开始";
-        inputbtn.onclick = rewards()
-        document.getElementById("b_header").appendChild(inputbtn);
+    //添加按钮
+    //开始
+    var inputbtn = document.createElement('input');
+    inputbtn.id="btn";
+    inputbtn.type="button";
+    inputbtn.value="开始";
+    inputbtn.onclick = rewards();
+    document.getElementById("b_header").appendChild(inputbtn);
 
-        //停止
-        var stopbtn = document.createElement('input');
-        stopbtn.id="stop_btn";
-        stopbtn.type="button";
-        stopbtn.value="停止";
-        stopbtn.onclick = function() {
-            window.open("about:blank","_self")
-            window.close();
-            alert("请手动关闭标签页!");
-        };
-        document.getElementById("b_header").appendChild(stopbtn);
+    //停止
+    var stopbtn = document.createElement('input');
+    stopbtn.id="stop_btn";
+    stopbtn.type="button";
+    stopbtn.value="停止";
+    stopbtn.onclick = function() {
+        i=100;
+        //location.href="cn.bing.com";
+        alert("请手动禁用脚本");
+    };
+    document.getElementById("b_header").appendChild(stopbtn);
 
-        // Your code here...
-    })();
+    // Your code here...
+})();
